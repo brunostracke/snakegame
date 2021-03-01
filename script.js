d4d26d051b22e95280af848bee400dd227cfacb7
brunostracke <brunostracke@gmail.com>
2021-03-01 20:42:16 -0300

    criando a comidinha da cobrinha

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ snake[0] = {
 }
 let direction = "right";
 
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG () {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -18,6 +23,11 @@ function criarCobrinha () {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function drawFood () {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -56,6 +66,7 @@ function iniciarJogo () {
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
